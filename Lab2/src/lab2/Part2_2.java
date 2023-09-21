4472142e7539643bbbb0bd23257c76c4878333d0
iamthanng <iamthanng@gmail.com>
2023-09-21 16:40:38 +0700

    Workshop2

---
+++ Lab2/src/lab2/Part2_2.java
@@ -0,0 +1,32 @@
+
+package lab2;
+
+import java.util.Scanner;
+
+public class Part2_2 {
+    public String inputString() throws Exception
+    {
+        String pattern="SE\\d{3}";
+        String s="";
+        Scanner sc=new Scanner(System.in);
+        System.out.println("input the string:");
+        s=sc.nextLine();
+        if(!s.matches(pattern))
+            throw new Exception();
+        return s;
+    }
+    public static void main(String[] args) {
+        Part2_2 obj=new Part2_2();
+        boolean cont=false;
+        do{
+            try{
+                String s=obj.inputString();
+                System.out.println("the string is " + s);
+                cont=false;
+            } catch(Exception e) {
+                System.out.println("The string is invalid");
+                cont=true;
+            }
+        } while(cont);
+    }
+}
